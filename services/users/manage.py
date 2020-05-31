a416ca25fa4d17076605aeeb4a5f094d0e446636
n4djib <n4djib@gmail.com>
2020-06-01 00:23:55 +0100

    created users service

---
+++ services/users/manage.py
@@ -0,0 +1,14 @@
+from flask.cli import FlaskGroup
+from project import create_app
+# from project.api.models import User
+
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+
+
+
+
+if __name__ == '__main__':
+    cli()
